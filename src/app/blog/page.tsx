b0bf6f400a6479c694bc3c1bad3da1ebeb6c8a4b
Han <ahsj22@naver.com>
2025-01-06 01:32:47 +0900

    Adding Error Handling in Blog Page

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,8 +1,28 @@
 import { RepoData } from "@/types/repo";
 
+async function getProjects() {
+  try {
+    const response = await fetch("http://localhost:3001/repos");
+    if (!response.ok) throw new Error();
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Failed to fetch projects:", error);
+    return [];
+  }
+}
+
 export default async function Page() {
-  const response = await fetch("http://localhost:3001/repos");
-  const projects: RepoData[] = await response.json();
+  const projects: RepoData[] = await getProjects();
+
+  if (!projects.length) {
+    return (
+      <p className="bg-red-500 text-2xl text-white font-bold  m-4 p-10">
+        {"서버 Error 발생"}
+      </p>
+    );
+  }
 
   return (
     <article className="border-4 border-blue-500 m-4 p-10">
