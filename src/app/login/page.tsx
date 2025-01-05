b255cbea758eea6de56e7210f38cd3ab1259c826
Han <ahsj22@naver.com>
2025-01-06 02:27:47 +0900

    Adding SignUp Form in Login Page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,10 @@
+import SignUpForm from "@/components/sign-up-form";
+
 export default function page() {
   return (
     <article className="border-4 border-orange-500 m-4 p-10">
       <h1>Page: Login</h1>
+      <SignUpForm />
     </article>
   );
 }
