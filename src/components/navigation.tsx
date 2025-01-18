8b8d2b35b79357f7d6906a0cf417a9db6a1d2f13
Han <ahsj22@naver.com>
2025-01-18 12:52:09 +0900

    Adding Style in Navigation Component

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav className="border-4 border-green-500">
-      <ul className="flex">
-        <li className="flex-none mr-2">
+    <nav className="font-mono">
+      <ul className="flex flex-col md:flex-row">
+        <li className="md:mr-5">
           <Link href="/">Home</Link>
         </li>
-        <li className="flex-none mr-2">
+        <li className="md:mr-5">
           <Link href="/about">About</Link>
         </li>
-        <li className="flex-none mr-2">
+        <li className="md:mr-5">
           <Link href="/about/projects">Projects</Link>
         </li>
-        <li className="flex-none ml-auto mr-2">
+        <li className="md:flex-none md:ml-auto md:mr-5">
           <Link href="/login">Login</Link>
         </li>
       </ul>
