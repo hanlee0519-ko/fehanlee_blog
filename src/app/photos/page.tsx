d177f6db01fbbff2890afbcf58348db262c871a2
Han <ahsj22@naver.com>
2025-02-02 12:56:06 +0900

    After Using Next.js Image Component in Project

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,4 +1,9 @@
-import React from "react";
+import Image from "next/image";
+
+import dog1 from "./../../../public/images/dog1.png";
+import dog2 from "./../../../public/images/dog2.png";
+import dog3 from "./../../../public/images/dog3.png";
+import dog4 from "./../../../public/images/dog4.png";
 
 export default function PhotosPage() {
   return (
@@ -7,16 +12,32 @@ export default function PhotosPage() {
 
       <ul className="grid grid-cols-2 gap-4">
         <li className="h-60 overflow-hidden">
-          <img className="object-cover w-full h-full" src="/images/dog1.png" />
+          <Image
+            className="object-cover w-full h-full"
+            src={dog1}
+            alt="Photo of my dog"
+          />
         </li>
         <li className="h-60 overflow-hidden">
-          <img className="object-cover w-full h-full" src="/images/dog2.png" />
+          <Image
+            className="object-cover w-full h-full"
+            src={dog2}
+            alt="Photo of my dog"
+          />
         </li>
         <li className="h-60 overflow-hidden">
-          <img className="object-cover w-full h-full" src="/images/dog3.png" />
+          <Image
+            className="object-cover w-full h-full"
+            src={dog3}
+            alt="Photo of my dog"
+          />
         </li>
         <li className="h-60 overflow-hidden">
-          <img className="object-cover w-full h-full" src="/images/dog4.png" />
+          <Image
+            className="object-cover w-full h-full"
+            src={dog4}
+            alt="Photo of my dog"
+          />
         </li>
       </ul>
     </article>
